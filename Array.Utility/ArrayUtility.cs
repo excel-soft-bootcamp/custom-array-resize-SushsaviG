be820a9c5e8eedc92152f391cd364348ff14e311
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-07-05 16:32:46 +0530

    Add files via upload

---
+++ Array.Utility/ArrayUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayUtility
+{
+    public class ArrayUtility
+    {
+        int[] array;
+        
+
+
+        public ArrayUtility()
+        {
+            array = new int[5];
+        }
+
+        public static void Resize(ref int[] array, int newSize)
+        {
+           
+            // growing array
+                int[] arr = new int[array.Length + 2];
+            
+            //copying data to the new array
+            for (int i = 0; i < array.Length; i++)
+            {
+                arr[i] = array[i];
+            }
+            array = arr;
+
+
+
+
+
+
+        }
+    }
+}
